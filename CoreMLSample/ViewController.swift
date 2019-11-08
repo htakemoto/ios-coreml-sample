d89fbed2a615904812d27069da33607d994dacc3
Hiroshi Takemoto <hiroshi.takemoto@outlook.com>
2019-11-08 14:46:21 +0900

    supported more options for config

--- CoreMLSample/ViewController.swift
+++ CoreMLSample/ViewController.swift
@@ -10,81 +10,206 @@ import UIKit
 import AVKit
 import Vision
 
-class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+class ViewController: UIViewController {
 
     override var prefersHomeIndicatorAutoHidden: Bool {
         return true
     }
     
+    // MARK: - Properties
+    
+    @IBOutlet weak var previewView: UIView!
     @IBOutlet weak var belowView: UIView!
     @IBOutlet weak var objectNameLabel: UILabel!
     @IBOutlet weak var accuracyLabel: UILabel!
     
-    var model = Resnet50().model
+    var bufferSize: CGSize = .zero
+    var rootLayer: CALayer! = nil
+    private let captureSession = AVCaptureSession()
+    private var previewLayer: AVCaptureVideoPreviewLayer! = nil
+    private let videoDataOutput = AVCaptureVideoDataOutput()
+    private let videoDataOutputQueue = DispatchQueue(label: "VideoDataOutput", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
+    
+    // Vision parts
+    private var requests = [VNRequest]()
+
+    // MARK: - View Controller Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        //camera
-        let captureSession = AVCaptureSession()
+        // setup camera
+        setupAVCapture()
         
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
-        captureSession.addInput(input)
+        // setup Vision
+        setupVision()
         
+        // setup UI
+        setupUI()
+        
+        // start the capture
         captureSession.startRunning()
+    }
+
+    // MARK: - Setup Video
+    
+    func setupAVCapture() {
+        var deviceInput: AVCaptureDeviceInput!
         
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        view.layer.addSublayer(previewLayer)
-        previewLayer.frame = view.frame
-        // The camera is now created!
+        // Select a video device, make an input
+        let videoDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back).devices.first
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: videoDevice!)
+        } catch {
+            print("Could not create video device input: \(error)")
+            return
+        }
         
-        view.addSubview(belowView)
+        // FPS config
+        videoDevice?.activeVideoMinFrameDuration = CMTimeMake(value: 1, timescale: 30)
         
+        captureSession.beginConfiguration()
+        captureSession.sessionPreset = .vga640x480 // Model image size is smaller.
+        
+        // Add a video input
+        guard captureSession.canAddInput(deviceInput) else {
+            print("Could not add video device input to the session")
+            captureSession.commitConfiguration()
+            return
+        }
+        captureSession.addInput(deviceInput)
+        if captureSession.canAddOutput(videoDataOutput) {
+            captureSession.addOutput(videoDataOutput)
+            // Add a video data output
+            videoDataOutput.alwaysDiscardsLateVideoFrames = true
+            videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+            videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        } else {
+            print("Could not add video data output to the session")
+            captureSession.commitConfiguration()
+            return
+        }
+        let captureConnection = videoDataOutput.connection(with: .video)
+        // Always process the frames
+        captureConnection?.isEnabled = true
+        do {
+            try videoDevice!.lockForConfiguration()
+            let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
+            bufferSize.width = CGFloat(dimensions.width)
+            bufferSize.height = CGFloat(dimensions.height)
+            videoDevice!.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+        captureSession.commitConfiguration()
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        rootLayer = previewView.layer
+        previewLayer.frame = rootLayer.bounds
+        rootLayer.addSublayer(previewLayer)
+    }
+    
+    func startCaptureSession() {
+        captureSession.startRunning()
+    }
+    
+    // Clean up capture setup
+    func teardownAVCapture() {
+        previewLayer.removeFromSuperlayer()
+        previewLayer = nil
+    }
+    
+    // MARK: - Setup Vision (Core ML)
+    
+    @discardableResult
+    func setupVision() -> NSError? {
+        // Setup Vision parts
+        let error: NSError! = nil
+        
+        guard let modelURL = Bundle.main.url(forResource: "Resnet50", withExtension: "mlmodelc") else {
+            return NSError(domain: "ViewController", code: -1, userInfo: [NSLocalizedDescriptionKey: "Model file is missing"])
+        }
+        do {
+            let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: modelURL))
+            let objectRecognition = VNCoreMLRequest(model: visionModel, completionHandler: { (request, error) in
+                DispatchQueue.main.async(execute: {
+                    // perform all the UI updates on the main queue
+                    if let results = request.results {
+                        self.drawVisionRequestResults(results)
+                    }
+                })
+            })
+            self.requests = [objectRecognition]
+        } catch let error as NSError {
+            print("Model loading went wrong: \(error)")
+        }
+        
+        return error
+    }
+    
+    func drawVisionRequestResults(_ results: [Any]) {
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        
+        guard let results = results as? [VNClassificationObservation] else {return}
+        guard let firstObservation = results.first else {return}
+        // Select only the label with the highest confidence.
+        let name: String = firstObservation.identifier
+        let acc: Int = Int(firstObservation.confidence * 100)
+        self.objectNameLabel.text = name
+        self.accuracyLabel.text = "Accuracy: \(acc)%"
+        
+        CATransaction.commit()
+    }
+    
+    // MARK: - Orientation
+    
+    public func exifOrientationFromDeviceOrientation() -> CGImagePropertyOrientation {
+        let curDeviceOrientation = UIDevice.current.orientation
+        let exifOrientation: CGImagePropertyOrientation
+        
+        switch curDeviceOrientation {
+        case UIDeviceOrientation.portraitUpsideDown:  // Device oriented vertically, home button on the top
+            exifOrientation = .left
+        case UIDeviceOrientation.landscapeLeft:       // Device oriented horizontally, home button on the right
+            exifOrientation = .upMirrored
+        case UIDeviceOrientation.landscapeRight:      // Device oriented horizontally, home button on the left
+            exifOrientation = .down
+        case UIDeviceOrientation.portrait:            // Device oriented vertically, home button on the bottom
+            exifOrientation = .up
+        default:
+            exifOrientation = .up
+        }
+        return exifOrientation
+    }
+
+}
+
+// MARK: - UI
+extension ViewController {
+    fileprivate func setupUI() {
         belowView.clipsToBounds = true
         belowView.layer.cornerRadius = 15.0
         belowView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
-        
-        
-        let  dataOutput = AVCaptureVideoDataOutput()
-        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
-        captureSession.addOutput(dataOutput)
+        view.addSubview(belowView)
     }
-    
+}
+
+// MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
         
-        guard let model = try? VNCoreMLModel(for: model) else { return }
-        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
-            
-            guard let results = finishedReq.results as? [VNClassificationObservation] else {return}
-            guard let firstObservation = results.first else {return}
-            
-            let name: String = firstObservation.identifier
-            let acc: Int = Int(firstObservation.confidence * 100)
-            
-            DispatchQueue.main.async {
-                self.objectNameLabel.text = name
-                self.accuracyLabel.text = "Accuracy: \(acc)%"
-            }
-            
+        let exifOrientation = exifOrientationFromDeviceOrientation()
+
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: exifOrientation, options: [:])
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
         }
-        
-        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
-        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # iOS CoreML Sample
+
+## Requirements
+
+* Xcode 11.2+
+* iOS 12.0+
+* Swift 4.2
