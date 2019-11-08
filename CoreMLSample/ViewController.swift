b25f0f521231d931e5f92f87996b5bfa1b89614a
Hiroshi Takemoto <hiroshi.takemoto@outlook.com>
2019-11-08 12:38:04 +0900

    added initial Xcode project

---
+++ CoreMLSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLSample
+//
+//  Created by Hiroshi Takemoto on 11/8/19.
+//  Copyright © 2019 Hiroshi Takemoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
